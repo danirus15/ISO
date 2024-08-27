8a5a39c97fd98e20e7aec1ee7e3dc4b8415f7e1e
Danirus <me@danirus.com>
2024-08-27 20:09:03 +0700

    update slide hero
    update slide hero and responsive

--- src/App.tsx
+++ src/App.tsx
@@ -22,12 +22,22 @@ const App = () => {
     autoplay: true,
     autoplaySpeed: 5000,
     fade: true, // Optional: for fade transition
+    responsive: [
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      }
+    ]
   };
 
   function CardArticle(props) {
     return (
-      <div className="w-1/3 transition text-center hover:bg-slate-100 pb-3">
-        <img src={props.image} alt="" />
+      <div className="w-full md:basis-1/3 transition text-center hover:bg-slate-100 pb-3">
+        <img src={props.image} alt="" className="w-full"/>
         <h3 className="p-2 text-blue-950 font-medium text-xl">{props.title}</h3>
         <div className="mb-4 px-2 text-left">{props.shortDesc}</div>
         <a href="Learn more" className="btn-arrow text-color text-blue-950 font-medium">Learn more</a>
@@ -37,7 +47,7 @@ const App = () => {
 
   function CardServices(props) {
     return (
-      <div className="w-1/3 transition text-center px-3 py-5" style={{backgroundColor:"rgba(30, 86, 142, 0.47)"}}>
+      <div className="w-full md:basis-1/3 transition text-center px-3 py-5 mb-3" style={{backgroundColor:"rgba(30, 86, 142, 0.47)"}}>
         <div className="flex justify-center pt-10 pb-4">
           <img src={props.image} alt="" className="w-1/5"/>
         </div>
@@ -62,16 +72,40 @@ const App = () => {
       
 
       <header className="relative text-white overflow-hidden">
-        <img 
+        <Slider {...settings} className="w-full">
+          {heroImages.map((image, index) => (
+            <div
+              key={index}
+              className="w-full h-full flex items-center justify-center"
+            >
+              <img
+                src={image}
+                alt={`Hero ${index + 1}`}
+                className="w-full h-[400px] md:h-full object-cover"
+              />
+              <div className="hero-slide-text bottom-28 md:bottom-32 font-title text-3xl md:text-5xl md:w-[50%] md:left-[25%]">Where Innovation Meets Energy Excellence</div>
+            </div>
+          ))}
+        </Slider>
+        <div className="bottom-0 left-0 w-full bg-navy text-white py-2 overflow-hidden breaking-news-running">
+            <div className="whitespace-nowrap animate-marquee backdrop-blur-sm">
+              <p className="inline-block px-4">
+                Breaking News: New energy solutions unveiled! | Breaking News:
+                Major renewable energy breakthrough! | Breaking News:
+                Industry-leading innovations coming soon!
+              </p>
+            </div>
+          </div>
+        {/* <img 
             src={heroImage1}
-        />
+        /> */}
         <div className="text-hero bg-[#003366]">
           <div className="container mx-auto flex justify-between items-center p-10">
-            <div className="flex justify-between">
-              <div className="w-1/2">
+            <div className="md:flex md:flex-row">
+              <div className="md:w-1/2 pb-6">
                 <h1 className="font-title text-5xl">Where Innovation Meets Energy Excellence</h1>
               </div>
-              <div  className="w-1/2">
+              <div  className="md:w-1/2">
                 <div>Penton is more than a service provider; we're your trusted energy partner. With our expertise, reliability, and personalized approach, we're committed to helping you achieve your energy objectives efficiently and effectively. Partner with us and unlock your full potential in energy.
                 </div>
                 <div className="pt-10 pb-4">
@@ -85,12 +119,12 @@ const App = () => {
       </header>
       <div className="container mx-auto flex justify-between items-center p-10">
         <section>
-          <div className="w-3/4">
+          <div className="md:w-3/4">
             <div className="text-md text-orange-500 text-xl font-medium">Expertise</div>
             <h2 className="text-4xl text-blue-950 font-medium py-3 leading-snug">Comprehensive Consulting Services for Energy Projects</h2>
             <div>We provide a range of services to support energy projects, including architectural, structural, and mechanical drawings for gas stations and filling plants. We also offer energy regulatory compliance services, environmental impact assessments, and quotations for general engineering works.</div>
           </div>
-          <div className="flex justify-between my-10 gap-10 mb-16">
+          <div className="md:flex md:flex-row my-10 gap-10 mb-16">
               <CardArticle 
                 image="/src/assets/images/cards/card-1.png"
                 title="Architectural, Structural & Mechanical Drawings"
@@ -113,22 +147,22 @@ const App = () => {
 
       <section className="bg-[#003366] text-white">
         <div className="container mx-auto items-center p-10">
-            <div className=" justify-center">
-              <h2 className="text-4xl  px-64 text-center font-normal ">Streamline your operations with our Technical Audits and Compliance Services</h2>
+            <div className="justify-center">
+              <h2 className="text-4xl  px-6 md:px-64 text-center font-normal ">Streamline your operations with our Technical Audits and Compliance Services</h2>
             </div>
-            <div className="flex justify-between my-10 gap-10 mb-16">
+            <div className="md:flex md:flex-row my-10 gap-10 mb-16">
               <CardServices 
                 image="/src/assets/images/cards/service-1.png"
                 title="Ensure regulatory compliance with our Licenses and Permits Acquisition"
                 shortDesc="Our dedicated team of experienced experts is committed to working closely with you to develop a comprehensive and effective Emergency Response Plan that is tailored to your specific needs and requirements."
               />
                <CardServices 
-                image="/src/assets/images/cards/service-1.png"
+                image="/src/assets/images/cards/service-2.png"
                 title="Mitigate environmental risks with our Environmental Impact Assessments and Audits"
                 shortDesc="We specialize in providing comprehensive EIA/EA (Environmental Impact Assessment/Environmental Assessment) services for various energy and oil & gas projects, including but not limited to feasibility studies, impact mitigation strategies, stakeholder engagement, and regulatory compliance assessments."
               />
                <CardServices 
-                image="/src/assets/images/cards/service-1.png"
+                image="/src/assets/images/cards/service-3.png"
                 title="Get accurate quotations for your general engineering and energy works"
                 shortDesc="Our dedicated team of experienced professionals is committed to providing you with a range of competitive quotations that are customized to perfectly meet your specific needs and requirements."
               />
@@ -137,19 +171,19 @@ const App = () => {
       </section>
 
       <section>
-        <div className="container mx-auto items-center p-10">
+        <div className="container mx-auto items-center md:p-10">
           <div className="container mx-auto  justify-between items-center p-10">
-            <div className="flex justify-between">
-              <div className="w-1/2">
+            <div className="md:flex md:flex-row">
+              <div className="md:w-1/2 mb-4">
                 <h2 className="text-4xl text-blue-950 font-normal mr-6">Providing top-quality consulting services for the oil and gas sector.</h2>
               </div>
-              <div  className="w-1/2">
+              <div  className="md:w-1/2">
                 <div>Our company is dedicated to delivering expert solutions in architectural, structural, and mechanical drawings for oil and gas projects. We also specialize in energy regulatory compliance, environmental impact assessments, and offering quotations for general works. With a focus on excellence and customer satisfaction, we are committed to providing the highest level of service in the industry.
                 </div>
               </div>
             </div>
               <div className="w-full my-10">
-                <img src="/src/assets/images/banner/section-provide.png" alt="" />
+                <img src="/src/assets/images/banner/section-provide.png" alt="" className="w-full"/>
               </div>
           </div>
         </div>
@@ -164,15 +198,16 @@ const App = () => {
       </section>
 
       <section>
-        <div className="container mx-auto items-center p-10">
+        <div className="container mx-auto items-center md:p-10">
           <div className="container mx-auto  justify-between items-center p-10">
-            <div className="flex justify-between">
-              <div className="w-1/2 mr-10">
+            <div className="md:flex md:flex-row">
+              <div className="md:w-1/2 mr-10 mb-6">
                 <div className="text-3xl text-blue-950 font-normal mr-8">Common</div>
                 <div className="pt-4 pb-10">Find answers to frequently asked questions about our consulting services in the oil and gas industry, regulatory compliance, and environmental assessments.</div>
                 <a href="#" className="transition border-2 border-yellow-400 bg-yellow-400 text-black py-4 px-6 w-[20%] text-center rounded-lg font-semibold hover:bg-yellow-500 mr-3">Contact Us</a>
+                <br /><br/><br/>
               </div>
-              <div  className="w-1/2">
+              <div  className="md:w-1/2">
                 <div className="text-3xl text-blue-950 font-normal mb-4">Frequent Asked Questions</div>
 
                 <CardFaq title="What is oil and gas consulting?"/>
@@ -187,10 +222,25 @@ const App = () => {
       </section>
 
 
-      <section className="mb-6">
-        <div className="container mx-auto items-center p-10 bg-[#003366] text-white rounded-lg">
-          <div className="flex flex-row justify-between">
-            <div className="basis-1/2 p-10 px-20">
+      <section className="mb-6 mx-6">
+        <div className="container mx-auto items-center p-4 md:p-10 bg-[#003366] text-white rounded-lg">
+          <div className="md:flex flex-row justify-between">
+            <div className="basis-1/2 p-6 md:order-1">
+              <div className="font-title text-5xl font-medium mb-3">
+                Get in <span className="text-yellow-400">Touch</span>
+              </div>
+              <div>We'd love to hear from you! Whether you have a question about our services, need assistance, or you're in the neighborhood and want to check in, feel free to reach out.</div>
+              <form action="" className="mt-6 md:pr-10">
+                <input type="text" placeholder="Name" className="bg-transparent p-3 w-full border rounded mt-5"/>
+                <input type="text" placeholder="email" className="bg-transparent p-3 w-full border rounded mt-5"/>
+                <input type="text" placeholder="Phone number" className="bg-transparent p-3 w-full border rounded mt-5"/>
+                <select name="" id=""  className="bg-transparent p-3 w-full border rounded mt-5">
+                  <option value="">Select service</option>
+                </select>
+                <input type="button" value="SEND" className="bg-yellow-500 text-black font-bold p-3 w-full  rounded mt-5 hover:bg-yellow-300"/>
+              </form>
+            </div>
+            <div className="basis-1/2 p-10 md:px-20  md:order-0">
               <div className="font-title text-2xl font-bold">Contact Information</div>
               <div>Let’s have a chat</div>
               <div className="pt-6 flex flex-row">
@@ -214,21 +264,6 @@ const App = () => {
                 <a href="#" className="mr-5" > <img src="/src/assets/images/icons/icon-sosmed.png" alt="" style={{height:30}}/></a>
                 <a href="#" className="mr-5" > <img src="/src/assets/images/icons/icon-sosmed.png" alt="" style={{height:30}}/></a>
               </div>
-            </div>
-            <div className="basis-1/2 p-6">
-              <div className="font-title text-5xl font-medium mb-3">
-                Get in <span className="text-yellow-400">Touch</span>
-              </div>
-              <div>We'd love to hear from you! Whether you have a question about our services, need assistance, or you're in the neighborhood and want to check in, feel free to reach out.</div>
-              <form action="" className="mt-6 pr-10">
-                <input type="text" placeholder="Name" className="bg-transparent p-3 w-full border rounded mt-5"/>
-                <input type="text" placeholder="email" className="bg-transparent p-3 w-full border rounded mt-5"/>
-                <input type="text" placeholder="Phone number" className="bg-transparent p-3 w-full border rounded mt-5"/>
-                <select name="" id=""  className="bg-transparent p-3 w-full border rounded mt-5">
-                  <option value="">Select service</option>
-                </select>
-                <input type="button" value="SEND" className="bg-yellow-500 text-black font-bold p-3 w-full  rounded mt-5 hover:bg-yellow-300"/>
-              </form>
             </div>
           </div>
         </div>
